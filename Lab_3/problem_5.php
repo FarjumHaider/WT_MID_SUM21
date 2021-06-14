061094a55746ef12afa34f5f286a1c24a9d47e44
Farjum <yeanfarjum13@gmail.com>
2021-06-14 10:30:05 +0600

    lab_3_complete

--- Lab_3/problem_5.php
+++ Lab_3/problem_5.php
@@ -4,10 +4,10 @@ $backColor="green"; $fontColor="red"; $imageWidth="80px"; $imageHeight="80px";
 <table>
   <tr>
     <td>
-    <img src="aiub_logo.jpg" alt="Girl in a jacket" width="$imageWidth" height="$imageHeight">
+    <img src="aiub_logo.jpg" alt="Girl in a jacket" width="<?php echo "$imageWidth"; ?>" height="<?php echo "$imageHeight"; ?>">
     </td>
     <td>
-    <h2>American International University -<span style="background-color:green;color:red">Bangladesh </h2></span>
+    <h2>American International University -<span style="background-color:<?php echo "$backColor"; ?>;color:<?php echo " $fontColor"; ?>">Bangladesh </h2></span>
     </td>
   </tr>
 </table>
